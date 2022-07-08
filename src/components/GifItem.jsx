7b3b1927fe0ce5922591f9440f6a3e15834df02d
Beto <robledo.mdp@gmail.com>
2022-07-08 09:34:11 -0300

    fix img

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -3,7 +3,7 @@ export const GifItem = ({title, url}) => {
   
     return (
         <div className="card">
-            <img src={url} alt="gif" />
+            <img src={url} alt="gif" loading="lazy"/>
             <p>{title}</p>
         </div>
     )
